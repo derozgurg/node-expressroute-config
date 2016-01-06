7a57f4aa2bbf5e6deffd91ac0a149cad7fa17365
derozgur <derozgur@gmail.com>
2016-01-06 17:49:54 +0200

    fix

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-module.exports.applyRouteConfig = function (app,routConfig,disposeAfter) {
-    for(var i = 0 ; i < routConfig.methods.length;i++){
-        var route = routConfig.methods[i];
-        if(route.method.toLowerCase() == "get"){
-            app.get(route.path,routConfig.middleWare,route.controller);
-        }
-        else if(route.method.toLowerCase() == "post"){
-            app.post(route.path,routConfig.middleWare,route.controller);
-        }
-        else if(route.method.toLowerCase() == "delete"){
-            app.delete(route.path,routConfig.middleWare,route.controller);
+module.exports = function(app) {
+   "use strict";
+    return {
+        applyRouteConfig:(routeConfig, disposeAfter) => {
+            if (app && routeConfig) {
+                for (var i = 0; i < routeConfig.methods.length; i++) {
+                    var route = routeConfig.methods[i];
+                    if (route.method.toLowerCase() == "get") {
+                        app.get(route.path, routeConfig.middleWare, route.controller);
+                    }
+                    else if (route.method.toLowerCase() == "post") {
+                        app.post(route.path, routeConfig.middleWare, route.controller);
+                    }
+                    else if (route.method.toLowerCase() == "delete") {
+                        app.delete(route.path, routeConfig.middleWare, route.controller);
+                    }
+                }
+
+
+                if (disposeAfter) {
+                    for(var item in routeConfig ) delete routeConfig[item];
+                    routeConfig = null;
+                }
+            }
         }
     }
-
-
-    if(disposeAfter) routConfig == null;
 }
